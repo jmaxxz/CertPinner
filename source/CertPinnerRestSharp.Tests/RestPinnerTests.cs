a3fe7c655ad66ae6c17e6f728464ffe7d7990f28
jmaxxz <jmaxxz@users.noreply.github.com>
2018-08-13 22:36:14 -0500

    Layout Project

---
+++ source/CertPinnerRestSharp.Tests/RestPinnerTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using RestSharp;
+
+namespace CertPinnerRestSharp.Tests
+{
+	[TestFixture]
+	public class RestPinnerTests
+	{
+		[Test]
+		public void AddsCertificateValidatorToRestClient()
+		{
+			// Arrange
+			var restClient = new RestClient();
+			var restPinner = new RestPinner();
+
+			// Act
+			restPinner.EnablePinning(restClient);
+
+			// Assert
+			Assert.IsNotNull(restClient.RemoteCertificateValidationCallback);
+		}
+	 }
+ }
---
+++ source/CertPinnerRestSharp/RestPinner.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using RestSharp;
+
+namespace CertPinnerRestSharp
+{
+	public class RestPinner
+	{
+		private readonly bool _trustOnFirstuse;
+		private readonly bool _trustCertificateAuthorities;
+		private readonly bool _trustExpired;
+
+		public RestPinner(bool trustOnFirstuse = false, bool trustCertificateAuthorities = false, bool trustExpired = false)
+		{
+			_trustOnFirstuse = trustOnFirstuse;
+			_trustCertificateAuthorities = trustCertificateAuthorities;
+			_trustExpired = trustExpired;
+		}
+		public void EnablePinning(IRestClient restClient)
+		{
+			restClient.RemoteCertificateValidationCallback = RemoteCertificateValidationCallback;
+		}
+
+		private bool RemoteCertificateValidationCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslpolicyerrors)
+		{
+			return false;
+		}
+	}
+}
