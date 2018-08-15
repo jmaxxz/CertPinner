6ecf3597d1aded1f43035a12f1c457a5e56b8900
jmaxxz <jmaxxz@users.noreply.github.com>
2018-08-14 23:34:29 -0500

    Test keystore

---
+++ source/CertPinnerRestSharp.Tests/InMemoryKeyStoreTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace CertPinnerRestSharp
+{
+	[TestFixture]
+	public class InMemoryKeyStoreTests
+	{
+		[Test]
+		public void MatchesExistingPinOrIsNew_OnNew_ReturnsTrue()
+		{
+			// Arrange
+			var instance = new InMemoryKeyStore();
+			// Act
+			var result = instance.MatchesExistingPinOrIsNew("foo.com", new byte[] {0, 1, 2, 3});
+			// Assert
+			Assert.IsTrue(result);
+		}
+
+		[Test]
+		public void MatchesExistingPinOrIsNew_IfPublicKeyMatchesExisting_ReturnsTrue()
+		{
+			// Arrange
+			var instance = new InMemoryKeyStore();
+			instance.MatchesExistingPinOrIsNew("foo.com", new byte[] {0, 1, 2, 3});
+			// Act
+			var result = instance.MatchesExistingPinOrIsNew("foo.com", new byte[] {0, 1, 2, 3});
+			// Assert
+			Assert.IsTrue(result);
+		}
+
+		[Test]
+		public void MatchesExistingPinOrIsNew_IfPublicKeyDoesNotMatchExisting_ReturnsFalse()
+		{
+			// Arrange
+			var instance = new InMemoryKeyStore();
+			instance.MatchesExistingPinOrIsNew("foo.com", new byte[] {0, 1, 2, 3});
+			// Act
+			var result = instance.MatchesExistingPinOrIsNew("foo.com", new byte[] {0, 1, 2, 2});
+			// Assert
+			Assert.IsFalse(result);
+		}
+	}
+}
