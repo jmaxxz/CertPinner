c24f1b616d0b739bf39de5b23c13ad9c1480d038
jmaxxz <jmaxxz@users.noreply.github.com>
2018-08-17 20:25:05 -0500

    Remove restsharp

--- source/CertPinner.Tests/CertificatePinnerTests.cs
+++ source/CertPinner.Tests/CertificatePinnerTests.cs
@@ -2,7 +2,7 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using NUnit.Framework;
-using RestSharp;
+
 // ReSharper disable AccessToDisposedClosure
 // Disabled because AssertThrowsAsync waits so resharper misidentifies this
 namespace CertPinner
@@ -60,7 +60,7 @@ namespace CertPinner
 
 		[Category("Integration")]
 		[Test]
-		public async Task WhenTrustOnFirstUse_FirstRequest_ResultsInSuccess()
+		public void WhenTrustOnFirstUse_FirstRequest_ResultsInSuccess()
 		{
 			// Arrange
 			using (var restClient = GetClient("https://google.com"))
